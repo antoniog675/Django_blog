fabe222ebe70347ec51ada46e8e63872d1130aad
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-01 10:48:15 +0000

    Added templates for project and css in static folder

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    querySet = Post.objects.filter(status=1).order_by('-created_by')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
