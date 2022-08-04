99f05a44a0d556cdf39415d76356c373fb261e0c
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-04 11:42:25 +0000

    Changes to blog/urls and tests

--- blog/admin.py
+++ blog/admin.py
@@ -22,5 +22,5 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-    def approve_comments(self, request, querySet):
+    def approve_comments(self, request, queryset):
         queryset.update(approved=True)
